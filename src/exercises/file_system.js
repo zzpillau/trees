69eadd9bc8710e6e0160cf235342db23f3ba21a4
zzpillau <zzpilla@gmail.com>
2024-01-06 21:41:46 +0200

    add file_system.js generator.js

---
+++ src/exercises/file_system.js
@@ -0,0 +1,9 @@
+// Урок "Виртуальная файловая система"
+import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
+
+const tree = mkdir('etc', [
+  mkfile('bashrc'),
+  mkdir('consul', [mkfile('config.json')]),
+]);
+
+console.log(JSON.stringify(tree, null, ' '));
